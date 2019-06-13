1b95a1199a8d8848b4a741a91e614f09896d5f1d
SmartDost <mkgoyal.online@gmail.com>
2019-06-13 10:41:34 +0530

    again test

---
+++ src/org/cts/test/Employee.java
@@ -0,0 +1,18 @@
+package org.cts.test;
+
+public class Employee {
+
+	public void empId() {
+		// TODO Auto-generated method stub
+System.out.println("123");
+	}
+	
+
+
+public static void main(String[] args) {
+	
+	Employee e=new Employee();
+	e.empId();
+}
+
+}
